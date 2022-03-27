7439f893cf894c15573853256a9b7deec63d69bc
Yolanda Hunter <ythunter@hotmail.com>
2022-03-27 15:20:01 -0400

    Fixed file error

--- Election_Analysis/PyPoll_Challenge.py
+++ Election_Analysis/PyPoll_Challenge.py
@@ -5,9 +5,11 @@ import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("..", "Resources", "election_results.csv")
+#file_to_load = os.path.join("..", "Resources", "election_results.csv")
+file_to_load = "Resources/election_results.csv"
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+#file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = "analysis/election_analysis.txt"
 
 # Initialize a total vote counter.
 total_votes = 0
